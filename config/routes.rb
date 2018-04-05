91960fa004be3cbc494e8145239e9221cca97979
baleonly <baleonly@gmail.com>
2018-04-06 01:46:20 +0900

    update views and navigration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
 	root 'welcomes#index'
 	get 'my_portfolio', to: 'users#my_portfolio'
 	get 'search_stocks', to: 'stocks#search'
 	resources :user_stocks, only: [:create, :destroy]
-	
 end
